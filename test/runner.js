1c8d86340faa06c6ace094b1a02aee9e44cc9bcd
fuiyu <823825491@qq.com>
2016-09-07 10:59:20 +0800

    test 测试

---
+++ test/runner.js
@@ -0,0 +1,16 @@
+//rntests([f1,f2,f3,...], callback)
+
+module.exports = function runTests(fns,done){
+    var index = 0
+    function next(err){
+        if(err){
+            return done(err)
+        }
+        if(index >= fns.length){
+            return done()
+        }
+        console.log('index',index,fns[index].name)
+        fns[index++](next)
+    }
+    next()
+}
